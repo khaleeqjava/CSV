d1c3d18b78a61df1bda9db304db6ce43f879759b
Khaleeq-ur-Rahman <khaleeqjava@gmail.com>
2020-06-26 01:54:35 -0400

    Reading Data from CSV file.

--- Assignments/src/first/Test.java
+++ Assignments/src/first/Test.java
@@ -1,13 +1,33 @@
 package first;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
 		
 			PersonalInfo personalInfo = new PersonalInfo("Khaleeq","Rahman",18);
 			Address address = new Address("CortezCourt","L8Z 2Z9","Ottawa","Canada");
-			Customer customer = new Customer(50000,personalInfo,address);
+			//Customer customer = new Customer(50000,personalInfo,address);
 			
-			customer.display();
+			//customer.display();
+			
+			
+			try {
+				File customer = new File ("C:\\Users\\napev-PS\\git\\CSV\\Assignments\\src\\first\\customer.csv");
+				Scanner filereader = new Scanner (customer);
+				while(filereader.hasNextLine()){
+					String data = filereader.nextLine();
+					
+					System.out.println(data);
+										
+				}
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("Error");
+				e.printStackTrace();
+			}
 			}
 		 }
